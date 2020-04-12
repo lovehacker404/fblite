24ad57ddd7731640139a4312d53e8e22a5e02161
lovehacker <61407838+lovehackerAsif@users.noreply.github.com>
2020-04-12 18:22:50 +0500

    Update Crack.py

--- Crack.py
+++ Crack.py
@@ -965,9 +965,21 @@ if __name__ == '__main__':
                 WhatsApp Num
               \033[1;94m +923094161457""
 	
-		raw_input("\n\033[1;97m[\033[1;94mBack\033[1;97m]")
-	menu()
+		else:
+        if '"is_shielded":false' in res.text:
+            os.system('clear')
+            print logo
+            print 52 * '\x1b[1;97m\xe2\x95\x90'
+            print '\x1b[1;91m[\x1b[1;96m\xe2\x9c\x93\x1b[1;91m] \x1b[1;91mDeactivated'
+            raw_input('\n\x1b[1;91m[ \x1b[1;97mBack \x1b[1;91m]')
+            lain()
+        else:
+            print '\x1b[1;91m[!] Error'
+            keluar()
+
 
 if __name__ == '__main__':
 	login()
+
+
 
